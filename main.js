fab10ef18f31efde3f035aa86a06646202794e67
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-09-16 15:50:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,4 +22,28 @@ document.getElementById('result_confidence').style.color="rgb("+random_r+","+ran
 img1=document.getElementById("aliens-01");
 img2=document.getElementById("aliens-02");
 img3=document.getElementById("aliens-03");
-img4=document.getElementById("aliens-04");}}
+img4=document.getElementById("aliens-04");
+if(results[0].label=="Meow"){
+    img1.src="download.gif";
+    img2.src="download (1).jpg";
+    img3.src="download (2).jpg";
+    img4.src="download (2).png";
+}
+else if(results[0].label=="Bark"){
+    img1.src="depositphotos_43182117-stock-illustration-funny-cat-cartoon.jpg";
+    img2.src="download(2).gif";
+    img3.src="download (2).jpg";
+    img4.src="download (2).png";
+     }
+     else if(results[0].label=="Hiss"){
+        img1.src="depositphotos_43182117-stock-illustration-funny-cat-cartoon.jpg";
+        img2.src="download (1).jpg";
+        img3.src="download (1).gif";
+        img4.src="download (2).png";}
+
+        else if(results[0].label=="Moo"){
+            img1.src="depositphotos_43182117-stock-illustration-funny-cat-cartoon.jpg";
+            img2.src="download (1).jpg";
+            img3.src="download (2).jpg";
+            img4.src="download (3).gif";}
+        }}
