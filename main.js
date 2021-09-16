0e6cda9b6500853225984551574be8c95c291101
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-09-16 15:37:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// https://teachablemachine.withgoogle.com/models/ZDN7Pd3Ct/ //
+function startclassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/ZDN7Pd3Ct/',modelReady)
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){
+     if(error){
+         console.error(error);
+     }
+     else{
+         console.log(results);
+         random_r=Math.floor(Math.random()*255)+1;
+         random_b=Math.floor(Math.random()*255)+1;
+         random_g=Math.floor(Math.random()*255)+1;
+
+document.getElementById('result_label').innerHTML="i can hear-"+results[0].label;
+document.getElementById('result_confidence').innerHTML="accuracy-"+(results[0].confidence*100).toFixed(2)+"%";
+document.getElementById('result_label').style.color="rgb("+random_r+","+random_g+","+random_b+")";
+document.getElementById('result_confidence').style.color="rgb("+random_r+","+random_g+","+random_b+")";
+img1=document.getElementById("aliens-01");
+img2=document.getElementById("aliens-02");
+img3=document.getElementById("aliens-03");
+img4=document.getElementById("aliens-04");}}
